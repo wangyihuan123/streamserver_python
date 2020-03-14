789d0c1d7a6a7d0a4210244d7ced3fdee5ec0c0c
Ben Wang <ben.wang@c3.co.nz>
2020-03-15 11:36:10 +1300

    send image from client's webcam

--- client.py
+++ client.py
@@ -1,25 +1,41 @@
 import socket
+import cv2
+import time
 
+# client
 IP_SERVER = "0.0.0.0"
 PORT_SERVER = 953
 TIMEOUT_SOCKET = 10
-SIZE_PACKAGE = 4096
+DEVICE_NUMBER = 0
 
 
 if __name__ == '__main__':
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    connection.settimeout(TIMEOUT_SOCKET)
-    connection.connect((IP_SERVER, PORT_SERVER))
+    socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_client.settimeout(TIMEOUT_SOCKET)
+    socket_client.connect((IP_SERVER, PORT_SERVER))
 
+    conn = socket_client.makefile('wb')
+
+    camera = cv2.VideoCapture(DEVICE_NUMBER)
     count = 0
-    while True:
+
+    while(camera.isOpened()):
         try:
-            connection.sendall(b'Hello, world ' + str(count))
-            data = connection.recv(1024)
-            print('Received', repr(data))
+            ret, frame = camera.read()
+            # print("shape", frame.shape)  # frame is numpy array: (480, 640, 3)
+            byteImage = frame.tobytes()
+            conn.write(byteImage)
+
+            # todo: sendall or makefile->write?
+            # socket_client.sendall(byteImage)
+
+
             count += 1
+            print('Frame: ', count, 'Size:', len(byteImage))
+            time.sleep(10)
+
         except Exception as e:
             print("[Error] " + str(e))
 
-    connection.close()
+    socket_client.close()
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import socket
 from threading import Thread
 
+# server
 SERVER_IP = "0.0.0.0"
 SERVER_PORT = 953
 MAX_NUM_CONNECTIONS = 20
-DEVICE_NUMBER = 0
+# image
+IMAGE_HEIGHT = 480
+IMAGE_WIDTH = 640
+COLOR_PIXEL = 3  # RGB
 
 class ConnectionPool(Thread):
 
@@ -16,12 +20,14 @@ class ConnectionPool(Thread):
         print("[+] New server socket thread started for " + self.ip + ":" + str(self.port))
 
     def run(self):
+        count = 0
         try:
             while True:
-                data = self.conn.recv(1024)
+                data = self.conn.recv(IMAGE_HEIGHT * IMAGE_WIDTH * COLOR_PIXEL)
                 if not data:
                     break
-                self.conn.sendall(data)
+                print(count, len(data))
+                count += 1
         except Exception, e:
             print "Connection lost with " + self.ip + ":" + str(self.port) + "\r\n[Error] " + str(e.message)
         self.conn.close()
@@ -29,13 +35,13 @@ class ConnectionPool(Thread):
 
 if __name__ == '__main__':
     print("Waiting connections...")
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    connection.bind((SERVER_IP, SERVER_PORT))
-    connection.listen(MAX_NUM_CONNECTIONS)
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_server.bind((SERVER_IP, SERVER_PORT))
+    socket_server.listen(MAX_NUM_CONNECTIONS)
     while True:
-        (conn, (ip, port)) = connection.accept()
+        (conn, (ip, port)) = socket_server.accept()
         thread = ConnectionPool(ip, port, conn)
         thread.start()
-    connection.close()
+    socket_server.close()
     camera.release()
