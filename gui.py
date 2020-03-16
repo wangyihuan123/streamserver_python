f8cd535adf9548001ded6d1a0c50dc76cac287f2
Ben Wang <ben.wang@c3.co.nz>
2020-03-16 21:06:45 +1300

    add buttons and lable for gui

--- gui.py
+++ gui.py
@@ -10,8 +10,15 @@ class StreamServerGui:
         self.content_frame = ttk.Frame(master, style='new.TFrame')
         self.content_frame.pack(fill=BOTH, expand=True)
 
-        self.footer_frame = ttk.Frame(master,style='new.TFrame')
-        self.footer_frame.pack(fill=BOTH)
+        self.label1 = ttk.Label(self.content_frame, text="Please press the buttons to Stat or Stop recording.",font=("helvetica", 20),
+                                style='L1.TLabel').grid(row=0, column=0, columnspan=2, padx=5, pady=10,sticky='sw')
+
+        self.startButton = ttk.Button(self.content_frame, text="Start", style='B1.TButton',command=self.start_recording)
+        self.startButton.grid(row=2, column=0,  padx=5, pady=25, sticky='se')
+
+
+        self.stopButton = ttk.Button(self.content_frame, text="Stop", style='B1.TButton',command=self.stop_recording)
+        self.stopButton.grid(row=2, column=1,  padx=200, pady=25,sticky='se')
 
         load = Image.open("img.jpg")
         load = load.resize((250, 250), Image.ANTIALIAS)
@@ -19,6 +26,13 @@ class StreamServerGui:
         self.img = ttk.Label(self.content_frame, image=render)
         self.img.image = render
         self.img.place(x=500, y=0)
+
+
+    def start_recording(self):
+        print("start button pressed")
+
+    def stop_recording(self):
+        print("stop button pressed")
 
 
 def gui_config(self):
